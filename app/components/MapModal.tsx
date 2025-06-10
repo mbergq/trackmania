1a320e5f68f97d44db11cf4714dcf906c4adddd5
Martin <martinberg9@proton.me>
2025-06-10 23:42:19 +0200

    Add function to calculate personal best medal

--- app/components/MapModal.tsx
+++ app/components/MapModal.tsx
@@ -2,9 +2,43 @@ import { formatTime } from "@/lib/utils/client-utils";
 import type { getMapRecordsFn } from "@/server/getMapRecords";
 import { useNavigate } from "@tanstack/react-router";
 import { use } from "react";
+import authorMedal from "@/assets/medals/medal_author.png";
+import goldMedal from "@/assets/medals/medal_gold.png";
+import silverMedal from "@/assets/medals/medal_silver.png";
+import bronzeMedal from "@/assets/medals/medal_bronze.png";
 
 type Props = {
 	mapPromise: ReturnType<typeof getMapRecordsFn>;
+};
+
+const calculatePb = (
+	pb: number,
+	bronze: number,
+	silver: number,
+	gold: number,
+	author: number,
+) => {
+	const medal = () => {
+		if (pb <= author) {
+			return authorMedal;
+		}
+		if (pb <= gold) {
+			return goldMedal;
+		}
+		if (pb <= silver) {
+			return silverMedal;
+		}
+		if (pb <= bronze) {
+			return bronzeMedal;
+		}
+		return "No medal";
+	};
+
+	return (
+		<div>
+			<img className="w-6 h-6" src={medal()} alt="medal logo" />
+		</div>
+	);
 };
 
 export const MapModal: React.FC<Props> = ({ mapPromise }) => {
@@ -25,6 +59,13 @@ export const MapModal: React.FC<Props> = ({ mapPromise }) => {
 				<div key={x.mapId}>
 					<p>Your PB:{formatTime(x.recordScore.time)}</p>
 					<p>Record driven: {new Date(x.timestamp).toLocaleString()}</p>
+					{calculatePb(
+						x.recordScore.time,
+						mapInfo.bronzeScore,
+						mapInfo.silverScore,
+						mapInfo.goldScore,
+						mapInfo.authorScore,
+					)}
 				</div>
 			))}
 		</div>
