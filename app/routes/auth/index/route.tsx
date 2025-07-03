b3edcacd2e47b397fdc68a29340d48ce1287e592
Martin <martinberg9@proton.me>
2025-07-04 00:35:38 +0200

    Use react-hook-form

--- app/routes/auth/index/route.tsx
+++ app/routes/auth/index/route.tsx
@@ -7,6 +7,8 @@ import {
 	InputOTPSlot,
 } from "@/components/ui/input-otp";
 import { Input } from "@/components/ui/input";
+import { useForm, type SubmitHandler, Controller } from "react-hook-form";
+import { REGEXP_ONLY_DIGITS } from "input-otp";
 
 type Inputs = {
 	username: string;
@@ -24,33 +26,56 @@ export const Route = createFileRoute("/auth/")({
 
 function RouteComponent() {
 	const setSession = useServerFn(setSessionFn);
+	const {
+		register,
+		handleSubmit,
+		watch,
+		control,
+		formState: { errors },
+	} = useForm<Inputs>();
+	const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
 	return (
 		<div className="flex justify-center items-center h-dvh">
-			<form className="flex flex-col gap-y-2">
-				<span className="text-white font-mono">What can we call you?</span>
+			<form className="flex flex-col gap-y-2" onSubmit={handleSubmit(onSubmit)}>
+				<span className="text-white font-mono">Who are you then?</span>
 				<Input
 					className="border-gray-800 bg-gray-700 text-tm-green"
 					placeholder="Username.."
+					{...register("username")}
+					required={true}
 				/>
 				<span className="text-white font-mono">Passcode</span>
-				<InputOTP maxLength={8}>
-					<InputOTPGroup className="text-tm-green font-mono">
-						<InputOTPSlot index={0} />
-						<InputOTPSlot index={1} />
-						<InputOTPSlot index={2} />
-						<InputOTPSlot index={3} />
-						<InputOTPSlot index={4} />
-						<InputOTPSlot index={5} />
-						<InputOTPSlot index={6} />
-						<InputOTPSlot index={7} />
-					</InputOTPGroup>
-				</InputOTP>
+				<Controller
+					name="passcode"
+					control={control}
+					render={({ field }) => (
+						<InputOTP
+							maxLength={8}
+							pattern={REGEXP_ONLY_DIGITS}
+							value={field.value?.toString()}
+							onChange={field.onChange}
+							onBlur={field.onBlur}
+							ref={field.ref}
+						>
+							<InputOTPGroup className="text-tm-green font-mono">
+								<InputOTPSlot index={0} />
+								<InputOTPSlot index={1} />
+								<InputOTPSlot index={2} />
+								<InputOTPSlot index={3} />
+								<InputOTPSlot index={4} />
+								<InputOTPSlot index={5} />
+								<InputOTPSlot index={6} />
+								<InputOTPSlot index={7} />
+							</InputOTPGroup>
+						</InputOTP>
+					)}
+				/>
+				<button type="submit">Click</button>
 			</form>
 		</div>
 	);
 }
-
 /* <button
 	type="button"
 	onClick={() =>
