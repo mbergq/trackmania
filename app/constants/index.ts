2e7da0dcb5f5d5ff922f4fb9dd6b17c8fb3bf266
Martin <martinberg9@proton.me>
2025-07-04 00:08:52 +0200

    Add components for auth form

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,8 +1,9 @@
 import { createTrackmaniaClient } from "@/lib/trackmania";
 
 const ACCOUNT_ID = process.env.ACCOUNT_ID ?? "";
+const PASSCODE = process.env.PASSCODE ?? "";
 
 const { tmCoreClient } = createTrackmaniaClient("NadeoServices");
 const { tmLiveMeetClient } = createTrackmaniaClient("NadeoLiveServices");
 
-export { ACCOUNT_ID, tmCoreClient, tmLiveMeetClient };
+export { ACCOUNT_ID, PASSCODE, tmCoreClient, tmLiveMeetClient };
--- app/routes/auth/index/route.tsx
+++ app/routes/auth/index/route.tsx
@@ -1,6 +1,17 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 import { setSessionFn } from "@/server/setSession";
 import { useServerFn } from "@tanstack/react-start";
+import {
+	InputOTP,
+	InputOTPGroup,
+	InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { Input } from "@/components/ui/input";
+
+type Inputs = {
+	username: string;
+	passcode: number;
+};
 
 export const Route = createFileRoute("/auth/")({
 	component: RouteComponent,
@@ -15,20 +26,40 @@ function RouteComponent() {
 	const setSession = useServerFn(setSessionFn);
 
 	return (
-		<div>
-			<button
-				type="button"
-				onClick={() =>
-					setSession({
-						data: {
-							username: "martin",
-						},
-					})
-				}
-			>
-				Test me
-			</button>
-			Hello "/auth/"!
+		<div className="flex justify-center items-center h-dvh">
+			<form className="flex flex-col gap-y-2">
+				<span className="text-white font-mono">What can we call you?</span>
+				<Input
+					className="border-gray-800 bg-gray-700 text-tm-green"
+					placeholder="Username.."
+				/>
+				<span className="text-white font-mono">Passcode</span>
+				<InputOTP maxLength={8}>
+					<InputOTPGroup className="text-tm-green font-mono">
+						<InputOTPSlot index={0} />
+						<InputOTPSlot index={1} />
+						<InputOTPSlot index={2} />
+						<InputOTPSlot index={3} />
+						<InputOTPSlot index={4} />
+						<InputOTPSlot index={5} />
+						<InputOTPSlot index={6} />
+						<InputOTPSlot index={7} />
+					</InputOTPGroup>
+				</InputOTP>
+			</form>
 		</div>
 	);
 }
+
+/* <button
+	type="button"
+	onClick={() =>
+		setSession({
+			data: {
+				username: "martin",
+			},
+		})
+	}
+>
+	Test me
+</button> */
