380e3f25e5609e24b2eaf3deba451ffcf91d98d1
Martin <martinberg9@proton.me>
2025-07-09 23:48:10 +0200

    Use shadcn nav menu

--- app/routes/(app)/route.tsx
+++ app/routes/(app)/route.tsx
@@ -5,6 +5,13 @@ import {
 	redirect,
 } from "@tanstack/react-router";
 
+import {
+	NavigationMenu,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+
 export const Route = createFileRoute("/(app)")({
 	component: RouteComponent,
 	beforeLoad: ({ context }) => {
@@ -30,18 +37,31 @@ function RouteComponent() {
 				<div className="py-3">
 					<h1 className="text-2xl font-bold text-center mb-3">Trackmania</h1>
 					<nav className="flex justify-center gap-x-4">
-						<Link
-							to="/"
-							className="px-4 py-2 bg-gray-800 hover:bg-gray-700 rounded transition-colors duration-150 text-tm-green font-medium"
-						>
-							Home
-						</Link>
-						<Link
-							to="/records"
-							className="px-4 py-2 bg-gray-800 hover:bg-gray-700 rounded transition-colors duration-150 text-tm-green font-medium"
-						>
-							Records
-						</Link>
+						<NavigationMenu>
+							<NavigationMenuList>
+								<NavigationMenuItem>
+									<NavigationMenuLink asChild>
+										<Link
+											to="/"
+											className="bg-tm-grey border px-4 py-2 rounded-md inline-block"
+										>
+											Home
+										</Link>
+									</NavigationMenuLink>
+								</NavigationMenuItem>
+
+								<NavigationMenuItem>
+									<NavigationMenuLink asChild>
+										<Link
+											to="/records"
+											className="bg-tm-grey border px-4 py-2 rounded-md inline-block"
+										>
+											Records
+										</Link>
+									</NavigationMenuLink>
+								</NavigationMenuItem>
+							</NavigationMenuList>
+						</NavigationMenu>
 					</nav>
 				</div>
 			</header>
